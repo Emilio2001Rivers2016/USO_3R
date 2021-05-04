9fc285cdc07774f120e09f2d7e42ee9fe89a60de
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-04 00:13:36 -0500

    Added earth anim images

---
+++ Assets/Scripts/ToxicBarUpdate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToxicBarUpdate : MonoBehaviour {
+
+	[SerializeField] private Image[] levelBars = new Image[10];
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
