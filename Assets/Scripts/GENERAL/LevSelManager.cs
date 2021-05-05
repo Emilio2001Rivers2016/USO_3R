bf903034924a8b19603c539d96f83a65fb396f93
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-05 00:21:00 -0500

    Added graphics
    Added level selection buttons
    
    Added Recycle level basic graphic design

---
+++ Assets/Scripts/GENERAL/LevSelManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevSelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void gotoReduce() {
+    	SceneManager.LoadScene("Reduce");
+    }
+    public void gotoReuse() {
+    	SceneManager.LoadScene("Reuse");
+    }
+    public void gotoRecycle() {
+    	SceneManager.LoadScene("Recycle");
+    }
+}
--- Assets/Scripts/GENERAL/ToxicBarUpdate.cs
+++ Assets/Scripts/GENERAL/ToxicBarUpdate.cs
@@ -6,14 +6,27 @@ using UnityEngine.UI;
 public class ToxicBarUpdate : MonoBehaviour {
 
 	[SerializeField] private Image[] levelBars = new Image[10];
+	private int toxicLevel;
 
     // Start is called before the first frame update
     void Start() {
-        
+    	PlayerPrefs.SetInt("toxLev", (int) Random.Range(1,11)); //// SOLUTION THAT NEEDS TO BE CHANGED
+    	
+        toxicLevel = PlayerPrefs.GetInt("toxLev");
     }
 
     // Update is called once per frame
     void Update() {
-        
+    	activeColorBars(toxicLevel);
     }
+    
+    void activeColorBars(int val) {
+    	for(int i = 0; i < val; ++i) {
+    		levelBars[i].gameObject.SetActive(true);
+    	}
+    	for(int j = val; j < 10; j++) {
+    		levelBars[j].gameObject.SetActive(false);    	
+    	}
+    }
+    
 }
