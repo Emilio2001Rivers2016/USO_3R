60d2c40313b5345bdc9f3faccea0e7a60a300c93
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-13 23:17:30 -0500

    Added generic basic defeat screen

--- Assets/Scripts/GENERAL/DefeatScreenLogic.cs
+++ Assets/Scripts/GENERAL/DefeatScreenLogic.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefeatScreenLogic : MonoBehaviour {
 
+	[SerializeField] private Text gameScore, highScore;
+
 	// Start is called before the first frame update
 	void Start() {
-		Debug.Log(GameLogic.recycleScore);
-		Debug.Log(PlayerPrefs.GetInt("RecycleRecord"));
-
 		if(GameLogic.recycleScore > PlayerPrefs.GetInt("RecycleRecord")) {
 			PlayerPrefs.SetInt("RecycleRecord", GameLogic.recycleScore);
 		}
+		gameScore.text = "Puntaje de esta partida:\n" + GameLogic.recycleScore;
+		highScore.text = "Puntaje récord:\n" + PlayerPrefs.GetInt("RecycleRecord");
 	}
 
 	// Update is called once per frame
