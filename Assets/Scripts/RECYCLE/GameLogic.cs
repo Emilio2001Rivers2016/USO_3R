ba921f8d1187f241416e02d8cc0ccdcfb5dad29c
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-13 20:51:23 -0500

    (Recycle) Improved Tag System

--- Assets/Scripts/RECYCLE/GameLogic.cs
+++ Assets/Scripts/RECYCLE/GameLogic.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLogic : MonoBehaviour {
 
+	public static int recycleScore;
+	public static bool defeat;
+
 	[SerializeField] private GameObject TrashPrefab;
+	[SerializeField] private Text scoreText;
 
 	private float minX = 0-(Screen.width/2)+Screen.width*0.2f;
 	private float maxX = 0+(Screen.width/2)-Screen.width*0.2f;
@@ -13,13 +18,17 @@ public class GameLogic : MonoBehaviour {
 
 	// Start is called before the first frame update
 	void Start() {
+		// Resets necessary variables to the corresponding value
+		recycleScore = 0;
+		defeat = false;
 		// Starts creating trash
 		StartCoroutine("GenerateTrash");
 	}
 
 	// Update is called once per frame
 	void Update()  {
-
+		scoreText.text = "Puntaje: " + GameLogic.recycleScore;
+		if(defeat) Time.timeScale = 0;
 	}
 
 	private IEnumerator GenerateTrash() {
--- Assets/Scripts/RECYCLE/TrashBehaviour.cs
+++ Assets/Scripts/RECYCLE/TrashBehaviour.cs
@@ -8,24 +8,31 @@ public class TrashBehaviour : MonoBehaviour {
 	[SerializeField] private Sprite organicImg1, organicImg2, organicImg3,
 								inorganicImg1, inorganicImg2, inorganicImg3,
 								specialImg1, specialImg2, specialImg3;
-	private string thisTag;
+	private string thisTag, otherTag, otherTag_wrong1, otherTag_wrong2;
 
 	// Start is called before the first frame update
 	void Start() {
 		switch((int) Random.Range(0f, 3f)) {
 			case 0:
 				thisTag = "Organic";
+				otherTag_wrong1 = "InorganicLaser";
+				otherTag_wrong2 = "SpecialLaser";
 				RandomPick(organicImg1, organicImg2, organicImg3);
 				break;
 			case 1:
 				thisTag = "Inorganic";
+				otherTag_wrong1 = "OrganicLaser";
+				otherTag_wrong2 = "SpecialLaser";
 				RandomPick(inorganicImg1, inorganicImg2, inorganicImg3);
 				break;
 			case 2:
 				thisTag = "Special";
+				otherTag_wrong1 = "InorganicLaser";
+				otherTag_wrong2 = "OrganicLaser";
 				RandomPick(specialImg1, specialImg2, specialImg3);
 				break;
 		}
+		otherTag = thisTag+"Laser";
 		gameObject.tag = thisTag;
 		// Modifies both image size and box collider size
 		gameObject.GetComponent<BoxCollider2D>().size = new Vector2((float) Screen.width*0.1f, (float) Screen.height*0.1f);
@@ -39,12 +46,12 @@ public class TrashBehaviour : MonoBehaviour {
 		}
 	}
 
-
 	private void OnTriggerEnter2D(Collider2D other) {
-		if(gameObject.CompareTag(thisTag) && other.gameObject.CompareTag(thisTag)) {
+		if(gameObject.CompareTag(thisTag) && other.gameObject.CompareTag(otherTag)) {
 			Destroy(gameObject);
-		} else {
-			Debug.Log("You failed!!!");
+			GameLogic.recycleScore++;
+		} else if(other.gameObject.CompareTag(otherTag_wrong1) || other.gameObject.CompareTag(otherTag_wrong2)) {
+			GameLogic.defeat = true;
 		}
 	}
 
