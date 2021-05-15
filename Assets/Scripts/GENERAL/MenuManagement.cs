b7f62b20f4924291df8a477d9a492b319a082f90
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-14 23:34:24 -0500

    Began with main menu design improvement

--- Assets/Scripts/GENERAL/MenuManagement.cs
+++ Assets/Scripts/GENERAL/MenuManagement.cs
@@ -2,18 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManagement : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
-    
-    // Method to go to level selection
-    public void gotoLevSelect() { SceneManager.LoadScene("LevelSelection"); }
+	[SerializeField] private Image background;
+	[SerializeField] private Sprite goodBack, badBack;
+
+	// Start is called before the first frame update
+	void Start() {
+		StartCoroutine("AnimateBackground");
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	// Method to go to level selection
+	public void gotoLevSelect() { SceneManager.LoadScene("LevelSelection"); }
+
+	private IEnumerator AnimateBackground() {
+		while(true) {
+			background.GetComponent<Image>().sprite = goodBack;
+			background.canvasRenderer.SetAlpha(0);		
+			background.CrossFadeAlpha(1, 0.5f, false);
+			yield return new WaitForSeconds(2.5f);
+
+			background.canvasRenderer.SetAlpha(1);		
+			background.CrossFadeAlpha(0, 0.5f, false);
+			yield return new WaitForSeconds(0.5f);
+
+			background.GetComponent<Image>().sprite = badBack;
+			background.canvasRenderer.SetAlpha(0);		
+			background.CrossFadeAlpha(1, 0.5f, false);
+			yield return new WaitForSeconds(2.5f);
+
+			background.canvasRenderer.SetAlpha(1);		
+			background.CrossFadeAlpha(0, 0.5f, false);
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
