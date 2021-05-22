2ceac90ec05d4a45b4a248873f99e91f084a38e4
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-21 20:00:36 -0500

    Added basic door animations

---
+++ Assets/Scripts/REDUCE/DoorMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorMovement : MonoBehaviour {
+
+	private Animator animator;
+	[SerializeField] private int position;
+	
+	public int TRASH_POS;
+
+
+	// Start is called before the first frame update
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		makeTransition();
+	}
+
+	private void makeTransition() {
+		animator.SetInteger("FUTURE", TRASH_POS);
+	}
+
+	
+
+}
+
+
+
+
+
