cfa651e391b0994038105010f8c4a467625237d1
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-25 02:47:43 -0500

    Added Earth rotation Animation

--- Assets/Scripts/GENERAL/RotatingEarth.cs
+++ Assets/Scripts/GENERAL/RotatingEarth.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RotatingEarth : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class RotatingEarth : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField] private Sprite[] imageSet = new Sprite[10];
+
+	// Start is called before the first frame update
+	void Start() {
+		StartCoroutine("rotate");
+	}
+
+	private IEnumerator rotate() {
+		while(true) {
+			for(int i = 0; i < 10; i++) {
+				GetComponent<Image>().sprite = imageSet[i];
+				yield return new WaitForSeconds(0.25f);
+			}
+		}
+	}
 }
