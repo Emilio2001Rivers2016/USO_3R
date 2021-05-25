5f8e1c2ef387d9cfd17212cbfbfad3dd51282c0c
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-25 18:48:50 -0500

    Improved Reduce Level

--- Assets/Scripts/REDUCE/GameLogic_Reduce.cs
+++ Assets/Scripts/REDUCE/GameLogic_Reduce.cs
@@ -5,61 +5,67 @@ using UnityEngine.UI;
 
 public class GameLogic_Reduce : MonoBehaviour {
 
-	private int index, reduceScore, resources;
+	private int index, reduceScore, resources, time;
 	private string[] doors = new string[3];
 
-	[SerializeField] private Text scoreText;
+	[SerializeField] private Text scoreText, timeText;
 	[SerializeField] private Image defeatScreen;
 
 	// Start is called before the first frame update
 	void Start() {
 		// Resets necessary variables to the corresponding value
+		time = 5;
 		reduceScore = 0;
 		resources = 1;
 		doors[0] = "LeftDoor";
 		doors[1] = "MiddleDoor";
 		doors[2] = "RightDoor";
+		StartCoroutine("timeControl");
 	}
 
 	// Update is called once per frame
 	void Update() {
 		scoreText.text = "Puntaje:\n" + reduceScore;
+		timeText.text = time.ToString();
 		if(resources <= 0) {
 			defeatScreen.gameObject.SetActive(true);
 		}
 	}
 
-	public void gameShuffle() {
-		index = (int) Random.Range(0, 3);
-		Debug.Log(index);
-		DoorShuffle.instance.shuffle();
-	}
-
-	public void validateLeftClick() {
-		if(doors[index] == "LeftDoor") {
+	public void validateClick(string doorTag) {
+		if(doors[index] == doorTag) {
 			reduceScore++;
-			resources++;
+			if(resources < 3) resources++;
 		} else {
 			resources--;
 		}
 	}
 
-	public void validateMiddleClick() {
-		if(doors[index] == "MiddleDoor") {
-			reduceScore++;
-			resources++;
-		} else {
-			resources--;
-		}
-	}
-
-	public void validateRightClick() {
-		if(doors[index] == "RightDoor") {
-			reduceScore++;
-			resources++;
-		} else {
-			resources--;
+	private IEnumerator timeControl() {
+		while(true) {
+			for(int i = 0; i < 5; i++) {
+				time--;
+				yield return new WaitForSeconds(1);
+			}
+			DoorShuffle.instance.shuffle();
+			time = 5;
+			yield return new WaitForSeconds(2.5f);
+			for(int i = 0; i < 5; i++) {
+				time--;
+				yield return new WaitForSeconds(1);
+			}
+			time = 6;
 		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
