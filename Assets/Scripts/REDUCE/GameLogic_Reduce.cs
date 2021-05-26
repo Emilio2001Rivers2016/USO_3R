a7f4b4bfea009bf2182c41a63ecdc2d7074bda5a
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-05-25 22:23:04 -0500

    Modified Reduce level logic and health bar

--- Assets/Scripts/REDUCE/GameLogic_Reduce.cs
+++ Assets/Scripts/REDUCE/GameLogic_Reduce.cs
@@ -8,6 +8,7 @@ public class GameLogic_Reduce : MonoBehaviour {
 	public static int reduceScore;
 
 	private int index, resources, time;
+	private bool chooseStage, clicked;
 	private string[] doors = new string[3];
 
 	[SerializeField] private Text scoreText, timeText;
@@ -16,6 +17,8 @@ public class GameLogic_Reduce : MonoBehaviour {
 	// Start is called before the first frame update
 	void Start() {
 		// Resets necessary variables to the corresponding value
+		chooseStage = false;
+		clicked = false;
 		time = 5;
 		reduceScore = 0;
 		resources = 1;
@@ -33,14 +36,24 @@ public class GameLogic_Reduce : MonoBehaviour {
 			DefeatScreenLogic.levelFinished = "Reduce";
 			defeatScreen.gameObject.SetActive(true);
 		}
+		if(clicked) {
+			StopCoroutine("timeControl");
+			time = 6;
+			chooseStage = false;
+			clicked = false;
+			StartCoroutine("timeControl");
+		}
 	}
 
 	public void validateClick(string doorTag) {
-		if(doors[index] == doorTag) {
-			reduceScore++;
-			if(resources < 3) resources++;
-		} else {
-			resources--;
+		if(chooseStage && !clicked) {
+			if(doors[index] == doorTag) {
+				reduceScore++;
+				if(resources < 3) resources++;
+			} else {
+				resources--;
+			}
+			clicked = true;
 		}
 	}
 
@@ -50,14 +63,17 @@ public class GameLogic_Reduce : MonoBehaviour {
 				time--;
 				yield return new WaitForSeconds(1);
 			}
+			index = (int) Random.Range(0,3);
 			DoorShuffle.instance.shuffle();
 			time = 5;
 			yield return new WaitForSeconds(2.5f);
+			chooseStage = true;
 			for(int i = 0; i < 5; i++) {
 				time--;
 				yield return new WaitForSeconds(1);
 			}
 			time = 6;
+			chooseStage = false;
 		}
 	}
 
