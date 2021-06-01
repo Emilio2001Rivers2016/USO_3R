5c63b59f1bed34444afdfce56b5f313ae5f9b55b
RebecaRojas <58601451+RebecaRojas@users.noreply.github.com>
2021-05-31 22:11:01 -0500

    Reuse

--- Assets/Scripts/REUSE/tofo/New/EndGame.cs
+++ Assets/Scripts/REUSE/tofo/New/EndGame.cs
@@ -6,6 +6,8 @@ public class EndGame : MonoBehaviour
 {
     public float timer = 3;
     bool reduce = false;
+    bool inZone = false;
+    bool objectToLose = false;
     public GameObject DeadScene;
 
     void OnTriggerEnter2D (Collider2D col){
@@ -18,14 +20,24 @@ public class EndGame : MonoBehaviour
         
             reduce = false;
             timer = 3;
+            Debug.Log("Ya no estoy en la zona");
+            inZone = false;
         
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        inZone=true;
+        if(col.gameObject.GetComponent<Rigidbody2D>().velocity== new Vector2(0,0) )
+        {
+            objectToLose = true;
+        }
     }
     
      // Update is called once per frame
     void Update () {
 
         if (reduce == true) {
-            Debug.Log (timer);
+            //Debug.Log (timer);
              timer -= 1 * Time.deltaTime;
         }
 
@@ -35,6 +47,14 @@ public class EndGame : MonoBehaviour
             Time.timeScale = 0;
             reduce = false;
              //put teleport part here
+        }
+        if(inZone && objectToLose){
+            Debug.Log("estoy en la zona");
+            Debug.Log ("Perdiste");
+            DeadScene.SetActive(true);
+            Time.timeScale = 0;
+            reduce = false;
+
         }
     }
 }
--- Assets/Scripts/REUSE/tofo/New/controller.cs
+++ Assets/Scripts/REUSE/tofo/New/controller.cs
@@ -6,29 +6,37 @@ public class controller : MonoBehaviour
 {
     public Rigidbody2D rb;
     //private bool moving = false;
-    [SerializeField]
+    
     //public static bool spawnPermit = true;
     // Update is called once per frame
+    private Vector2 startTouchPosition,endTouchPosition;
     void Update()
     {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
+        }
+        if(Input.touchCount >0 && Input.GetTouch(0).phase==TouchPhase.Ended)
+        {
+            endTouchPosition = Input.GetTouch(0).position;
+            if((endTouchPosition.x<startTouchPosition.x))
+            {
+                //transform.position=new Vector2(transform.position.x - 1.75f,transform.position.y);
+                
+                rb.velocity = new Vector2(-2.0f,-1f);
+            }
+            if((endTouchPosition.x > startTouchPosition.x))
+            {
+                //transform.position = new Vector2(transform.position.x +1.75f,transform.position.y);
+                
+                rb.velocity = new Vector2(2.0f, -1f);
+            }
+           // if((endTouchPosition.y < startTouchPosition.y))
+           // {
+           //     rb.velocity += new Vector2(0f,-9.0f);
+           // }
+        }
         
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.velocity = new Vector2(3.0f, -1f);
-            //moving = true;
-        }
-
-        //Press the Down arrow key to move the RigidBody downwards
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.velocity = new Vector2(-3.0f,-1f);
-            //moving = true;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            rb.velocity += new Vector2(0f,-1.0f);
-            //moving = true;
-        } 
     }
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -51,7 +59,7 @@ public class controller : MonoBehaviour
     {
         rb.constraints = RigidbodyConstraints2D.FreezeAll;
             Debug.Log("Congele este y aparecere uno nuevo");
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         GameLogic2.SpawnPermit = true;
         
     }
